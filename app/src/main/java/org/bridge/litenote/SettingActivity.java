36dfcb8581c159a792fa6d785f1073868e9a994c
BridgeGeorge <1650730996@qq.com>
2015-11-14 00:48:13 +0800

    添加应用商店评价功能。

--- app/src/main/java/org/bridge/litenote/SettingActivity.java
+++ app/src/main/java/org/bridge/litenote/SettingActivity.java
@@ -2,7 +2,10 @@ package org.bridge.litenote;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingActivity extends Activity {
 
@@ -12,5 +15,17 @@ public class SettingActivity extends Activity {
         setContentView(R.layout.activity_setting);
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * 打开应用市场进行评价
+     *
+     * @param v
+     */
+    public void startCommitIntent(View v) {
+        String str = "market://details?id=" + getPackageName();
+        Intent localIntent = new Intent("android.intent.action.VIEW");
+        localIntent.setData(Uri.parse(str));
+        startActivity(localIntent);
     }
 }
